02a465094fe86adf802450ad090dd41f11d9ebb7
aarr <arar0529@gmail.com>
2020-01-19 16:18:05 +0900

    changed

--- python_template/basic.py
+++ python_template/basic.py
@@ -17,6 +17,8 @@ print(int(sTring))     # 整数変換
 print(float(string))   # 浮動小数点数に変換
 
 # 標準入力からの入力
+# 複数の入力を受け付けるのであれば、inputを繰り返し実装するのではなく
+# ループして、入力内容をリストに保持するのがよい。
 print("＞標準入力---------------")
 print("なにか入力してください。")
 input_str = input()
@@ -46,6 +48,87 @@ elif 1 :
     print("1")
 else :
     print("other")
+
+
+# リスト
+print("＞リスト ---------------")
+list_first_half = ["1", "2", "3", "4", "5"]
+list_second_half = ["6", "7", "8", "9", "10"]
+
+print("２要素目 : " + list_first_half[1])
+# indexのマイナス指定は最終項目からのカウントとなる
+print("最終項目 : " + list_first_half[-1])
+# ＞範囲指定
+# X:Y -> Xは含む。Yは含まない。いずれも省略可能
+list_part = list_first_half[1:3]
+print("要素数 : " + str(len(list_part))) 
+print("切出要素 : ", list_part[0], list_part[1], sep=",")
+# ＞リスト連結
+list_sum = list_first_half + list_second_half
+print("リスト連結 : ", end="")
+print(list_sum)
+# ＞リスト増幅
+list_tripple = list_sum * 3
+print("リスト３倍 : ", end="")
+print(list_tripple)
+# ＞要素削除
+# delは変数の削除にも利用できるが、殆どの場合リストの要素削除に使われる
+print("3倍リストサイズ : " + str(len(list_tripple)))
+del list_tripple[10:] # 10要素以降を削除
+print("3倍リストサイズ（削除後） : " + str(len(list_tripple)))
+# 要素存在確認（in / not in）
+contains_1 = "1" in list_first_half
+print("１が含まれているか : " + str(contains_1))
+contains_num1 = 1 in list_first_half
+print("数字１が含まれているか : " + str(contains_num1)) # 型が異なると含まれないと判断される
+contains_1 = "6" in list_first_half
+print("６が含まれているか : " + str(contains_1))
+# ＞変数への複数代入法
+# リストのサイズと、変数の個数が同じであること
+# それが異なる場合、エラーとなる
+list_sample = ["one", "two", "three"]
+list_arg1, list_arg2, list_arg3 = list_sample
+print("複数代入法 : ", list_arg1, list_arg2, list_arg3)
+try:
+    error_arg1, error_arg2 = list_sample
+except ValueError as exception:
+    print("複数代入エラー : ", end="")
+    print(exception)
+# ＞リスト提供メソッド
+list_sum.append("11")           # 操作対象のリストに要素を追加する
+list_sum.extend(["0", "-10"])   # + の場合、コピーされ新たなリストが作成される。extendは操作対象のリストに指定されたリストを追加する 
+list_sum.remove("1")            # delは要素番号を指定して削除、removeは要素値を指定して削除
+print("2は何番目に存在するか : " + str(list_sum.index("2")))
+print("操作後リスト : ", list_sum)
+print("8は何番目に存在するか（5 - 10の間） : " + str(list_sum.index("8", 5, 10)))
+list_sum.sort()
+print("操作後リスト（ソート）", list_sum)
+# ＞リスト風（文字列）
+# リストはミュータブルだが、文字列はイミュータブル（不変）
+str_like_list = "123"
+print(str_like_list, "-> 2文字目 : ", str_like_list[1])
+# ＞リスト風（タプル）
+# 文字列同様イミュータブル（不変）
+def editList(target, index, value) :
+    try:
+        target[index] = value
+    except TypeError as exception : 
+        print("更新エラー : ", end="")
+        print(exception)
+    else :
+        print("更新完了 : ", target)
+
+tuple_like_list = (12, "34", None)
+print(tuple_like_list, " -> 3要素目 : ", tuple_like_list[2])
+editList(tuple_like_list, 0, "X")
+# ＞リスト、タプルへの型変換
+# タプリを更新したい場合に、リストに変換して操作する
+# 更新させたくないリストをタプルにするのもあり。
+# しかしタプルにすると利用できるメソッドが異なるのでまたリストする必要が出てくるケースもあるだろう
+list_by_tuple = list(tuple_like_list)
+editList(list_by_tuple, 0, "X")
+tuple_by_list = tuple(list_sum)
+editList(tuple_by_list, 0, "X")
 
 # ループ文
 # continue/breakの利用可能
@@ -92,3 +175,7 @@ print("＞プログラム終了---------------")
 import sys
 sys.exit()
 print("ここにはたどり着かない")
+
+
+
+
--- python_template/function.py
+++ python_template/function.py
@@ -9,28 +9,16 @@ def func() :
     # returnを記載しない場合、returnだけ記載して返却値を記載しない場合、Noneが返却される
 
 # 引数、戻り値ありの関数
-def funcArgs(arg1, arg2) :
+def func_args(arg1, arg2) :
     arg1Message = "引数１：" + str(arg1)
     arg2Message = "引数２：" + str(arg2)
     print(arg1Message)
     print(arg2Message)
     return arg1Message + "、" + arg2Message
 
-# スコープ確認関数
-# 一度ローカルスコープで利用した変数を、グローバル変数として利用することはできない
-def confirmLocalGlobal(funcArg) :
-    arg = "local"
-    print("Scope確認（arg）：" + arg)
-    print("Scope確認（funcArg）：" + funcArg)
-    global globalArg
-    print("明示的グローバル変数表示（globalArg）" + str(globalArg))
-    globalArg = globalArg + " Changed"
-    print("明示的グローバル変数変更（globalArg）" + globalArg)
-
 res = func() # Noneが返却される
 print("結果はNone？：" + str(res == None))
-print("戻り値：" + funcArgs("AAAA", random.randint(1, 9)))
-
+print("戻り値：" + func_args("AAAA", random.randint(1, 9)))
 
 # キーワード変数
 print("＞関数（キーワード変数）---------------")
@@ -41,11 +29,47 @@ print("Hello", "World", sep=",", end="END\r\n")
 
 
 # スコープ
+print("＞スコープ ---------------")
+# スコープ確認関数
+# 一度ローカルスコープで利用した変数を、グローバル変数として利用することはできない
+def confirm_local_global(func_arg) :
+    arg = "local"
+    print("Scope確認（arg）：" + arg)
+    print("Scope確認（func_arg）：" + func_arg)
+    global global_arg
+    print("明示的グローバル変数表示（global_arg）" + str(global_arg))
+    global_arg = global_arg + " Changed"
+    print("明示的グローバル変数変更（global_arg）" + global_arg)
+
 # 考え方は他のスクリプト言語と同様
 # スコープが異なれば、同じ変数名を利用することは許容されている
-print("＞スコープ ---------------")
 arg = "Global"
-funcArg = "Global"
-globalArg = "Global"
-confirmLocalGlobal("local")  # グローバル変数更新を含む
-print("グローバル変更確認（globalArg）：" + globalArg)
+func_arg = "Global"
+global_arg = "Global"
+confirm_local_global("local")  # グローバル変数更新を含む
+print("グローバル変更確認（global_arg）：" + global_arg)
+
+
+# 例外処理
+print("＞例外処理 ---------------")
+# 割り算（サンプル関数）
+def divide(arg1, arg2) :
+    try:
+        result =  arg1 / arg2 
+    # except 例外 as 変数名: とし、発生した例外を利用することが可能
+    # exceptを続けていくことで、複数のエラーハンドリングが可能
+    except ZeroDivisionError as error:
+        print("ERROR : ", end="")
+        print(error)
+    # Exceptionを明示的に定義しないと、全ての例外を補足する。
+    except:
+        print("エラー発生！")
+    # 例外が発生しなかった場合の処理を記述可能
+    else:
+    # 必ず実行する
+        return result
+    finally:
+        print("Finally")
+
+print("10 / 3 = " + str(divide(10, 3)))
+print("10 / 0 = " + str(divide(10, 0)))   # エアーメッセージが表示され、戻り値としてはNoneとなる
